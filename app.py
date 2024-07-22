f4e384d43f8a638ade4c0ca00d863fa29c10baf7
VTS072Varun <varun@vivatechsolutions.io>
2024-07-22 15:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def parse_resume():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=3000, host='0.0.0.0')
