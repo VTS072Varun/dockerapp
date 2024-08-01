f553bc46213e17cd3f147ae60023a55b81c2204a
VTS072Varun <varun@vivatechsolutions.io>
2024-08-02 01:28:59 +0530

    Update Resume_Parser.py

--- Resume_Parser.py
+++ Resume_Parser.py
@@ -176,4 +176,4 @@ def parse_resume():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0", debug=True)
+    app.run(port=3000, host="0.0.0.0", debug=True)
